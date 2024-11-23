d32c171aee1f8cf48520d976f104ed041b7c7ca8
q1bczyk <bartekkubik7@gmail.com>
2024-11-23 17:24:42 +0100

    Logo add

--- client/src/app/modules/admin/components/admin-nav/admin-nav.component.ts
+++ client/src/app/modules/admin/components/admin-nav/admin-nav.component.ts
@@ -3,13 +3,14 @@ import { MenuItem } from 'primeng/api';
 import { MenuModule } from 'primeng/menu'; 
 import { NavItemType } from '../../types/ui/nav-item.type';
 import { navItems } from './nav-items/nav-item';
+import { LogoComponent } from "../../../shared/ui/logo/logo.component";
 
 @Component({
   selector: 'app-admin-nav',
   templateUrl: './admin-nav.component.html',
   styleUrl: './admin-nav.component.scss',
   standalone: true,
-  imports: [MenuModule],
+  imports: [MenuModule, LogoComponent, LogoComponent],
 })
 export class AdminNavComponent implements OnInit {
 
