b4758da249cf1cd639b94b755d7f456ebd7818f5
q1bczyk <bartekkubik7@gmail.com>
2024-11-29 20:35:47 +0100

    Navigation styled

--- client/src/app/modules/admin/components/admin-nav/admin-nav.component.ts
+++ client/src/app/modules/admin/components/admin-nav/admin-nav.component.ts
@@ -3,21 +3,26 @@ import { MenuModule } from 'primeng/menu';
 import { NavItemType } from '../../types/ui/nav-item.type';
 import { navItems } from './nav-items/nav-item';
 import { LogoComponent } from "../../../shared/ui/logo/logo.component";
+import { NavigationService } from '../../services/ui/navigation.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-nav',
   templateUrl: './admin-nav.component.html',
   styleUrl: './admin-nav.component.scss',
   standalone: true,
-  imports: [MenuModule, LogoComponent, LogoComponent],
+  imports: [MenuModule, LogoComponent, LogoComponent, CommonModule],
 })
 export class AdminNavComponent implements OnInit {
 
   links : NavItemType[] = navItems; 
  
-  constructor() { }
+  constructor(public navigationService : NavigationService) { 
+
+  }
 
   ngOnInit() {
   }
+
 
 }
---
+++ client/src/app/modules/admin/components/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavigationService } from '../../services/ui/navigation.service';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  constructor(public navigationService : NavigationService){}
+}
