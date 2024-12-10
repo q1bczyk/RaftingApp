99730045e869ecab8d2fa80683cd329a3edd948c
q1bczyk <bartekkubik7@gmail.com>
2024-12-10 19:28:04 +0100

    Opening date menagment

--- client/src/app/modules/admin/pages/system-settings-page/components/date-settings/date-settings.component.ts
+++ client/src/app/modules/admin/pages/system-settings-page/components/date-settings/date-settings.component.ts
@@ -1,12 +1,15 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbDatepickerModule, NgbTimepickerModule, NgbTimeStruct } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-date-settings',
   standalone: true,
-  imports: [NgbTimepickerModule],
+  imports: [NgbTimepickerModule, JsonPipe, FormsModule, NgbDatepickerModule],
   templateUrl: './date-settings.component.html',
   styleUrl: './date-settings.component.scss'
 })
 export class DateSettingsComponent {
-
+  time: NgbTimeStruct = { hour: 13, minute: 30, second: 0 };
 }
