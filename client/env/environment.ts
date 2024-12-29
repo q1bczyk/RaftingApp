841a1fe260815c9f2a7c53fe8b9aa71e7f13a27b
q1bczyk <bartekkubik7@gmail.com>
2024-12-29 18:49:48 +0100

    ENV update

--- client/env/environment.ts
+++ client/env/environment.ts
@@ -2,6 +2,6 @@ export const environment =
 {
     production : false,
     apiUrl : 'https://localhost:5001/api',
-
+    stripeKey : 'pk_test_51Mm1V8FABRjuUwJ8lYdzwcIrnVZk7kLXPwwEPccriDsf3kXWdIMTqJj4Ggbk5VMhWLD0YgQ72vnbyFJfGrurO55i00Oli2I0ca'
     // hubUrl : 'https://localhost:5001/hubs/'
 };
