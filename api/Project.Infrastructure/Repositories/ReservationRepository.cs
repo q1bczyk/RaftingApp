77ecddd4a204cf17d84393907ed5506d3c78d088
q1bczyk <bartekkubik7@gmail.com>
2025-01-09 17:46:27 +0100

    Backend filters update

--- api/Project.Infrastructure/Repositories/ReservationRepository.cs
+++ api/Project.Infrastructure/Repositories/ReservationRepository.cs
@@ -19,8 +19,9 @@ namespace Project.Infrastructure.Repositories
             DateTime? endDate = null,
             DateTime? specificDate = null,
             string lastNamePartial = null,
-            string reservationId = null 
-        ){
+            string reservationId = null
+        )
+        {
             var query = _context.Reservations
                 .Include(r => r.Payment)
                 .Include(r => r.ReservationEquipment)
@@ -32,17 +33,25 @@ namespace Project.Infrastructure.Repositories
 
             if (!string.IsNullOrWhiteSpace(lastNamePartial))
                 query = query.Where(r => r.BookerLastname.ToLower().Contains(lastNamePartial.ToLower()));
-            
+
             if (startDate.HasValue)
                 query = query.Where(r => r.ExecutionDate.Date >= startDate.Value.Date);
 
             if (endDate.HasValue)
                 query = query.Where(r => r.ExecutionDate.Date <= endDate.Value.Date);
 
-             if (specificDate.HasValue)
+            if (specificDate.HasValue)
             {
-                var targetDate = specificDate.Value.Date; 
+                var targetDate = specificDate.Value.Date;
                 query = query.Where(r => r.ExecutionDate.Date == targetDate);
+            }
+
+            if (!specificDate.HasValue && !endDate.HasValue && !startDate.HasValue && string.IsNullOrWhiteSpace(reservationId) && string.IsNullOrWhiteSpace(lastNamePartial))
+            {
+                DateTime todayUtc = DateTime.UtcNow.Date; 
+                DateTime tomorrowUtc = todayUtc.AddDays(1); 
+
+                query = query.Where(r => r.ExecutionDate >= todayUtc && r.ExecutionDate < tomorrowUtc);
             }
 
             query = query.OrderBy(r => r.ExecutionDate);
@@ -59,7 +68,7 @@ namespace Project.Infrastructure.Repositories
                 .Where(r => r.Id == id.ToString())
                 .FirstOrDefaultAsync();
 
-            if(reservation == null)
+            if (reservation == null)
                 throw new NotFoundException("Not found!");
 
             return reservation;
